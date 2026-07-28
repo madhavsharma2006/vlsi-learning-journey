daa3175f18f4e8f4c8d6f740b6f655255208e50b
Madhav Sharma <madhav2006p@gmail.com>
2026-07-28 13:44:41 +0530

    Refactor ripple carry adder to include carry input

--- ripple_carry_adder.v
+++ ripple_carry_adder.v
@@ -1,7 +1,15 @@
-module rca (input [3:0] a_rca, [3:0] b_rca , [3:0] c_rca , output [3:0] sum_rca , [3:0] carry_rca );
-wire w1,w2,w3 ; 
-FA fa1 ( a_rca  , b_rca  , c_rca  , sum_rca, w1  );
-FA fa2 ( a_rca  , b_rca , c_rca  , sum_rca , w2  );
-FA fa3 ( a_rca  , b_rca , c_rca  , sum_rca , w3  );
-FA fa4 ( a_rca  , b_rca  , c_rca  , sum_rca , carry_rca );
-endmodule 
+module rca(
+    input [3:0] a_rca, b_rca,
+    input cin,
+    output [3:0] sum_rca,
+    output cout
+);
+
+wire w1, w2, w3;
+
+FA fa1(a_rca[0], b_rca[0], cin, sum_rca[0], w1);
+FA fa2(a_rca[1], b_rca[1], w1,  sum_rca[1], w2);
+FA fa3(a_rca[2], b_rca[2], w2,  sum_rca[2], w3);
+FA fa4(a_rca[3], b_rca[3], w3,  sum_rca[3], cout);
+
+endmodule
