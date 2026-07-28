779c725cdc9766e86e728d6c06cae38baa46bd9a
Madhav Sharma <madhav2006p@gmail.com>
2026-07-28 13:24:00 +0530

    Update Full_adder.v

--- Full_adder.v
+++ Full_adder.v
@@ -1,8 +1,15 @@
 module full_adder (input a , b ,cin , output s , c  );
+
   wire w1 , w2 , w3 ; 
-  xor x1(S , a, b ,cin);
+
+  xor x1(s , a, b ,cin); 
+
   and a1 (w1 , a ,b );
+
   and a2 (w2 , a ,cin );
+
   and a3 (w3 ,b , cin );
-  or o1 (w1,w2,w3);
+
+  or o1 (c,w1,w2,w3); 
+
 endmodule
